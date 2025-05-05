085a82430b1b2867ef7d3c1d54b39cec14b7cf3d
Bill Sanderson <bill@towniebagels.com>
2025-05-05 07:48:40 -0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+// git init && git add . && git commit -m "Initial commit" && git remote add origin https://github.com/bsanderson1981/section6 && git branch -M main && git push -u origin main
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Container(
+              color: Colors.white,
+              child: Text('Hello')
+          ),
+        ),
+      ),
+    );
+  }
+}
